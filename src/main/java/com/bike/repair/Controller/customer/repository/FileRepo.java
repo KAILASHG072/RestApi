ad9dde5640848963e1867404789e46b79bff6126
KAILASHG072 <130126252+KAILASHG072@users.noreply.github.com>
2023-04-07 11:34:08 +0530

    Delete src directory

--- src/main/java/com/bike/repair/Controller/customer/repository/FileRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bike.repair.Controller.customer.repository;
-
-import com.bike.repair.Controller.customer.entity.FileDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileRepo extends JpaRepository<FileDB, String> {
-}
